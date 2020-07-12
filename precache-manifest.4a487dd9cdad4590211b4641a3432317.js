4e9bc7b15b8c4b3721a58ac450dbc584c679ba2e
Saurabh <55626361+Saurabh-FullStackDev@users.noreply.github.com>
2020-07-12 13:20:49 +0530

    Updates

---
+++ precache-manifest.4a487dd9cdad4590211b4641a3432317.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea1f7a60ff2915c40d4b1a9dfdffc60",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "cbf16ab43754c02d93c1",
+    "url": "/Portfolio/static/css/main.532c10f3.chunk.css"
+  },
+  {
+    "revision": "b7b73cecdd13dabe53b8",
+    "url": "/Portfolio/static/js/2.45fdd2fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.45fdd2fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf16ab43754c02d93c1",
+    "url": "/Portfolio/static/js/main.0c1fbaf6.chunk.js"
+  },
+  {
+    "revision": "34b75beae03838036c0c",
+    "url": "/Portfolio/static/js/runtime-main.1011e462.js"
+  },
+  {
+    "revision": "64f76c44c991d2603d9693c48cf8d787",
+    "url": "/Portfolio/static/media/github.64f76c44.svg"
+  },
+  {
+    "revision": "7eec9e93188cb0eb6c620b3818a465dd",
+    "url": "/Portfolio/static/media/mail.7eec9e93.svg"
+  },
+  {
+    "revision": "aeae8adc2d185eaeaef0699e31f7a007",
+    "url": "/Portfolio/static/media/triangles.aeae8adc.png"
+  },
+  {
+    "revision": "1b236b0f97a21e2f3d640d0d7e7d64e1",
+    "url": "/Portfolio/static/media/twitter.1b236b0f.svg"
+  }
+]);
