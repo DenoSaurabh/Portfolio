650922ca80cb8e8077ca0e95e9480e1f5cfd215f
Saurabh <55626361+Saurabh-FullStackDev@users.noreply.github.com>
2020-07-12 13:08:51 +0530

    Adding build files for Github Pages

---
+++ build/precache-manifest.70199464d35f1973966112c0de60a4cf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8863d5e9d93886e5b16882c8fabf60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8536e76c0f0a2632ceab",
+    "url": "/static/css/main.7e53cf9c.chunk.css"
+  },
+  {
+    "revision": "b7b73cecdd13dabe53b8",
+    "url": "/static/js/2.45fdd2fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45fdd2fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8536e76c0f0a2632ceab",
+    "url": "/static/js/main.0c1fbaf6.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  },
+  {
+    "revision": "64f76c44c991d2603d9693c48cf8d787",
+    "url": "/static/media/github.64f76c44.svg"
+  },
+  {
+    "revision": "7eec9e93188cb0eb6c620b3818a465dd",
+    "url": "/static/media/mail.7eec9e93.svg"
+  },
+  {
+    "revision": "aeae8adc2d185eaeaef0699e31f7a007",
+    "url": "/static/media/triangles.aeae8adc.png"
+  },
+  {
+    "revision": "1b236b0f97a21e2f3d640d0d7e7d64e1",
+    "url": "/static/media/twitter.1b236b0f.svg"
+  }
+]);
